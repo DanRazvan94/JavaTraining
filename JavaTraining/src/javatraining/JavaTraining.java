19560e0f956734d9c49757d0dd6c922961918495
dan_r <dan_r@192.168.0.227>
2022-05-25 19:22:02 +0300

    First comint to DanRazvan94 repository - Java Training

---
+++ JavaTraining/src/javatraining/JavaTraining.java
@@ -0,0 +1,19 @@
+/*
+ * Teste repository new created.
+ */
+package javatraining;
+
+/**
+ * test new created repository.
+ * @author dan_r
+ */
+public class JavaTraining {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is new message!");
+	}
+	
+}
