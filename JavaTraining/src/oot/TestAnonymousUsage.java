052828d3aa38da8598caafd4491c451adeeac3b7
dan_r <dan_r@192.168.0.227>
2022-06-01 11:34:13 +0300

    First commit to DanRazvan94 repository - Java Training

---
+++ JavaTraining/src/oot/TestAnonymousUsage.java
@@ -0,0 +1,19 @@
+/*
+ * Testing anonymous classes
+ */
+package oop;
+
+/**
+ *
+ * @author dan_r
+ */
+public class TestAnonymousUsage {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
