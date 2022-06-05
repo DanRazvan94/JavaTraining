eb04573c3d5281966f7363d71ba058e09d5ea55e
dan_r <dan_r@192.168.0.227>
2022-06-05 11:16:20 +0300

    First commit to DanRazvan94 repository - Java Training

--- JavaTraining/src/oot/FrozenClass.java
+++ JavaTraining/src/oot/FrozenClass.java
@@ -8,7 +8,7 @@ package oot;
  * @author dan_r
  */
 public final class FrozenClass {
-	private String[] validKeys = {"12we34","33sd45","6ty5u7"};
+	private final String[] validKeys = {"12we34","33sd45","6ty5u7"};
 	public boolean checkValidity(String key){
 		boolean isValid = false;
 		for(String currentKey:validKeys){	
